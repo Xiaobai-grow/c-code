54117c52a31a2a741ab7c5022c2bd22223c9ab02
teacherjie <143690072+teacherjie@users.noreply.github.com>
2023-10-01 17:43:17 +0800

    Update test.c
    测试

--- test.c
+++ test.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{
+    int i =0;
+    int count =0;
+    for(i=1000; i<=2000; i++)
+   {
+         if(i%4 == 0)
+          {
+          printf("%d ",i);  
+           count++; 
+          }   
+   }
+   printf("\ncount = %d\n",count);
+    return 0;
+}
